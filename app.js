7af4c085056566af5fbbf15979ddf8049eadaeac
BMellet <benzero.m@gmail.com>
2017-12-07 16:52:49 +0100

    success

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
+var nb = 1;
+var txt = ["arrette et vas dehors !"," vas faire tes devoirs!","achete une vie serieux"];
+
+
+
+function compteur ()
+{
+
+	$('.cake').click(function()
+		{
+			var alea = 0 + Math.floor(Math.random() * 2);
+			nb++;
+			$('span').html(nb);
+
+			if((nb%20)==0)
+			{
+				alert(txt[alea]);
+			}
+
+		});
+
+	
+
+}
+
+
+
 $(document).ready(function(){
-	//votre code
+
+	compteur();
 
 
 });
